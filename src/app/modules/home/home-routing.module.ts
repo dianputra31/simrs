997cce9f508227295401b399be705a2efc45a302
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-07-08 18:02:09 +0700

    catalog

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayoutComponent } from 'src/app/transaction/pages/layout/layout.component';
 import { HomeLayoutComponent } from './pages/home-layout/home-layout.component';
 
 const routes: Routes = [
 	{
 		path: '',
-		component: LayoutComponent,
-		children: [
-			{
-				path: '',
-				component: HomeLayoutComponent,
-            }
-		],
+		component: HomeLayoutComponent,
+		// children: [
+		// 	{
+		// 		path: '',
+		// 		component: HomeLayoutComponent,
+		// 	},
+		// ],
 	},
 ];
 
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,16 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { LayoutComponent } from 'src/app/transaction/pages/layout/layout.component';
+import { CatalogSectionComponent } from './components/catalog-section/catalog-section.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeLayoutComponent } from './pages/home-layout/home-layout.component';
-
-
+import { CatalogCardComponent } from './components/catalog-card/catalog-card.component';
 
 @NgModule({
-  declarations: [ LayoutComponent, HomeLayoutComponent],
-  imports: [
-    CommonModule,RouterModule, HomeRoutingModule
-  ]
+	declarations: [HomeLayoutComponent, CatalogSectionComponent, CatalogCardComponent],
+	imports: [CommonModule, RouterModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
