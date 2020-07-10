e4df2584eebffb36ef65201dfde7e5a785a85660
Dian Putra <dian.putra@narindo.com>
2020-07-10 15:42:17 +0700

    Modal Dialog: Location at Home

--- src/app/modules/home/components/location-section/location-section.component.ts
+++ src/app/modules/home/components/location-section/location-section.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { DialogLocationSectionComponent } from '../dialog-location-section/dialog-location-section.component';
+
 
 @Component({
-  selector: 'app-location-section',
-  templateUrl: './location-section.component.html',
-  styleUrls: ['./location-section.component.scss']
+	selector: 'app-location-section',
+	templateUrl: './location-section.component.html',
+	styleUrls: ['./location-section.component.scss']
 })
 export class LocationSectionComponent implements OnInit {
 
-  constructor() { }
+	constructor(public dialog: MatDialog) { }
 
-  ngOnInit() {
-  }
+	openDialogLocation() {
+		// const dialogRef = this.dialog.open(DialogLocationSectionComponent);
+
+		// dialogRef.afterClosed().subscribe(result => {
+		// 	console.log(`Dialog result: ${result}`);
+		// });
+
+		const dialogConfig = new MatDialogConfig();
+		dialogConfig.disableClose = false;
+		dialogConfig.id = "modal-component";
+		dialogConfig.height = "313px";
+		dialogConfig.width = "680px";
+		dialogConfig.panelClass = "border-radius:20px";
+		dialogConfig.data = {
+			'searchId': 'hello'
+		}
+		const modalDialog = this.dialog.open(DialogLocationSectionComponent, dialogConfig);
+
+
+	}
+
+	ngOnInit() {
+	}
+
+
 
 }
