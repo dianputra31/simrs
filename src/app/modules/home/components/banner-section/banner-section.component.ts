7b9add0b1838213d871b728c191756fcfab34bbe
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-07-13 20:16:32 +0700

    arrow banner

--- src/app/modules/home/components/banner-section/banner-section.component.ts
+++ src/app/modules/home/components/banner-section/banner-section.component.ts
@@ -1,4 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { throwError } from 'rxjs';
+
+export interface PhotosApi {
+	albumId?: number;
+	id?: number;
+	title?: string;
+	url?: string;
+	thumbnailUrl?: string;
+}
 
 @Component({
 	selector: 'banner-section',
@@ -6,7 +17,42 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./banner-section.component.scss'],
 })
 export class BannerSectionComponent implements OnInit {
-	constructor() {}
+	apiData: PhotosApi;
+	limit: number = 2;
+	number = 590; // <==== Edit this number to limit API results
+	customOptions: OwlOptions = {
+		loop: true,
+		autoplay: true,
+		center: true,
+		dots: false,
+		autoHeight: true,
+		autoWidth: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 1,
+			},
+			1000: {
+				items: 1,
+			},
+		},
+	};
 
-	ngOnInit(): void {}
+	constructor(private readonly http: HttpClient) {}
+
+	ngOnInit() {
+		this.fetch();
+	}
+
+	fetch() {
+		const api = `https://jsonplaceholder.typicode.com/albums/1/photos?_start=0&_limit=${this.limit}`;
+		const http$ = this.http.get<PhotosApi>(api);
+
+		http$.subscribe(
+			(res) => (this.apiData = res),
+			(err) => throwError(err)
+		);
+	}
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { RouterModule } from '@angular/router';
+import { OwlModule } from 'ngx-owl-carousel';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { BannerSectionComponent } from './components/banner-section/banner-section.component';
 import { CatalogCardButtonComponent } from './components/catalog-card-button/catalog-card-button.component';
 import { CatalogCardComponent } from './components/catalog-card/catalog-card.component';
 import { CatalogSectionComponent } from './components/catalog-section/catalog-section.component';
@@ -10,7 +13,6 @@ import { DialogLocationSectionComponent } from './components/dialog-location-sec
 import { LocationSectionComponent } from './components/location-section/location-section.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeLayoutComponent } from './pages/home-layout/home-layout.component';
-import { BannerSectionComponent } from './components/banner-section/banner-section.component';
 
 @NgModule({
 	declarations: [
@@ -22,8 +24,16 @@ import { BannerSectionComponent } from './components/banner-section/banner-secti
 		DialogLocationSectionComponent,
 		BannerSectionComponent,
 	],
-	imports: [CommonModule, RouterModule, HomeRoutingModule, FormsModule, MatDialogModule],
+	imports: [
+		CommonModule,
+		RouterModule,
+		HomeRoutingModule,
+		FormsModule,
+		MatDialogModule,
+		CarouselModule,
+		OwlModule,
+	],
 	exports: [DialogLocationSectionComponent],
 	entryComponents: [DialogLocationSectionComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
