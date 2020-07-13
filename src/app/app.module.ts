7b297f8d6f30210b0a7da2fb5a1b2ec4e41a51e2
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-07-13 13:15:16 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { LayoutModule } from './layout/layout.module';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-import { AuthService } from './core/auth/service/auth.service';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 import { AuthModule } from './core/auth/auth.module';
+import { AuthService } from './core/auth/service/auth.service';
+import { LayoutModule } from './layout/layout.module';
+import { MaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    AuthModule,
-    BrowserModule,
-    LayoutModule,
-    RouterModule.forRoot([], {
-      scrollPositionRestoration: 'top'
-    }),
-    BrowserAnimationsModule,
-    MaterialModule
-  ],
-  providers: [AuthService],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [
+		AuthModule,
+		BrowserModule,
+		LayoutModule,
+		RouterModule.forRoot([], {
+			scrollPositionRestoration: 'top',
+		}),
+		BrowserAnimationsModule,
+		MaterialModule,
+	],
+	providers: [AuthService],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
