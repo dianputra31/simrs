51f29bb8eb7fac7c1865879dd07e41895bb55122
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-07-22 13:29:11 +0700

    wsh wsv

---
+++ src/app/shared/components/white-space-horizontal/white-space-horizontal.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'wsh',
+	templateUrl: './white-space-horizontal.component.html',
+	styleUrls: ['./white-space-horizontal.component.scss'],
+})
+export class WhiteSpaceHorizontalComponent implements OnInit {
+	@Input() wide: number;
+	constructor() {}
+	stylesObj = {};
+
+	ngOnInit(): void {
+		this.stylesObj = { height: this.wide };
+	}
+}
---
+++ src/app/shared/components/white-space-vertical/white-space-vertical.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'wsv',
+	templateUrl: './white-space-vertical.component.html',
+	styleUrls: ['./white-space-vertical.component.scss'],
+})
+export class WhiteSpaceVerticalComponent implements OnInit {
+	@Input() wide: number;
+	constructor() {}
+	stylesObj = {};
+
+	ngOnInit(): void {
+		this.stylesObj = { width: this.wide };
+	}
+}
