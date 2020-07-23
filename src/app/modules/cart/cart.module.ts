e93165cfd5eacfd3f1c3081d11efc24fd234348a
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-07-23 13:52:13 +0700

    import shared module

--- src/app/modules/cart/cart.module.ts
+++ src/app/modules/cart/cart.module.ts
@@ -1,15 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
 import { CartRoutingModule } from './cart-routing.module';
 import { CartLayoutComponent } from './pages/cart-layout/cart-layout.component';
 
-
 @NgModule({
-  declarations: [CartLayoutComponent],
-  imports: [
-    CommonModule,
-    CartRoutingModule
-  ]
+	declarations: [CartLayoutComponent],
+	imports: [CommonModule, CartRoutingModule, SharedModule],
 })
-export class CartModule { }
+export class CartModule {}
--- src/app/modules/cart/pages/cart-layout/cart-layout.component.ts
+++ src/app/modules/cart/pages/cart-layout/cart-layout.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'cart-layout',
-  templateUrl: './cart-layout.component.html',
-  styleUrls: ['./cart-layout.component.scss']
+	selector: 'cart-layout',
+	templateUrl: './cart-layout.component.html',
+	styleUrls: ['./cart-layout.component.scss'],
 })
 export class CartLayoutComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
