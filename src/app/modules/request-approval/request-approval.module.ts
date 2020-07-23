04d7ad99e5ef73b31f429d993d478face6493320
Dian Putra <dian.putra@narindo.com>
2020-07-23 19:14:49 +0700

    Address Request Approval

--- src/app/modules/request-approval/request-approval.module.ts
+++ src/app/modules/request-approval/request-approval.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { RequestApprovalLayoutComponent } from './pages/request-approval-layout/request-approval-layout.component';
 import { RequestApprovalRoutingModule } from './request-approval-routing.module';
+import { RequestApprovalAddressComponent } from './components/request-approval-address/request-approval-address.component';
 
 @NgModule({
-	declarations: [RequestApprovalLayoutComponent],
+	declarations: [RequestApprovalLayoutComponent, RequestApprovalAddressComponent],
 	imports: [CommonModule, RequestApprovalRoutingModule, SharedModule],
 })
 export class RequestApprovalModule {}
