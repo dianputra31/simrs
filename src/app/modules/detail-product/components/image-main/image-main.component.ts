87a27b63243768e3275ec4ef4fe606bbb89ca385
Hendrianto Cahyono <hendriantodc@gmail.com>
2020-07-31 04:27:21 +0700

    update

--- src/app/modules/detail-product/components/image-main/image-main.component.ts
+++ src/app/modules/detail-product/components/image-main/image-main.component.ts
@@ -23,15 +23,14 @@ export class ImageMainComponent implements OnInit {
 
 	openDialogImage(imageMainSelected) {
 		this.imageMainSelected = imageMainSelected
-		console.log(this.imageMainSelected);
 		const dialogConfig = new MatDialogConfig();
 		dialogConfig.disableClose = false;
 		dialogConfig.id = "modal-component";
 		dialogConfig.height = "auto";
-		dialogConfig.width = "330px";
+		dialogConfig.width = "350px";
 		//dialogConfig.panelClass = "border-radius:0px";
 		dialogConfig.data = {
-			//'images': { 'https://s2.bukalapak.com/img/24267287201/large/Cottonology_Wells_Black.jpg', 'https://s4.bukalapak.com/img/42609287201/large/Cottonology_Wells_Black.jpg', 'https://s3.bukalapak.com/img/81978287201/large/Cottonology_Wells_Black.jpg', 'https://s1.bukalapak.com/img/63918119411/large/Cottonology_Wells_Black.jpg'},
+			'images': ['https://s2.bukalapak.com/img/24267287201/large/Cottonology_Wells_Black.jpg', 'https://s4.bukalapak.com/img/42609287201/large/Cottonology_Wells_Black.jpg', 'https://s3.bukalapak.com/img/81978287201/large/Cottonology_Wells_Black.jpg', 'https://s1.bukalapak.com/img/63918119411/large/Cottonology_Wells_Black.jpg'],
 			'imageMainSelected': this.imageMainSelected,
 		}
 		const modalDialog = this.dialog.open(DialogPopupImageComponent, dialogConfig);
--- src/app/shared/components/dialog-popup-image/dialog-popup-image.component.ts
+++ src/app/shared/components/dialog-popup-image/dialog-popup-image.component.ts
@@ -10,12 +10,34 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 export class DialogPopupImageComponent implements OnInit {
 
 	imageMainSelected = '';
+	images = [];
+	index = 0;
+	i = 0;
 
 	constructor(private dialogRef: MatDialogRef<DialogPopupImageComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
 		this.imageMainSelected = data.imageMainSelected;
+		this.images.push(data.images)
 	}
 
 	ngOnInit(): void {
 	}
+
+	eventNav(action, imageMainSelected) {
+		this.index = this.images[0].indexOf(imageMainSelected);
+		var i = this.index;
+		if (action == 'next') {
+			i = i + 1;
+			i = i % this.images[0].length;
+			this.imageMainSelected = this.images[0][i];
+		}
+		else {
+			if (i === 0) {
+				i = this.images[0].length;
+			}
+			i = i - 1;
+			this.imageMainSelected = this.images[0][i];
+		}
+	}
+
 
 }
