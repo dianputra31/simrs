ab441b4a6a8e96f960e3bb902797dc6a55dc66b1
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-08-04 12:46:48 +0700

    filter-dropdown

---
+++ src/app/modules/approval/components/reset-filter-button/reset-filter-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'reset-filter-button',
+  templateUrl: './reset-filter-button.component.html',
+  styleUrls: ['./reset-filter-button.component.scss']
+})
+export class ResetFilterButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
