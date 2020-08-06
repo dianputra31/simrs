d24eb1d8a5b230f0766db803de2b99ffda9e6ded
Dian Putra <dian.putra@narindo.com>
2020-08-06 13:15:37 +0700

    New Address Notif

--- src/app/modules/approval/approval.module.ts
+++ src/app/modules/approval/approval.module.ts
@@ -4,11 +4,13 @@ import { NgbDropdownModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SharedModule } from '../../shared/shared.module';
 import { ApprovalRoutingModule } from './approval-routing.module';
 import { AddressButtonNotifComponent } from './components/address-button-notif/address-button-notif.component';
+import { AddressNotifComponent } from './components/address-notif/address-notif.component';
 import { ItemSummaryDropdownComponent } from './components/item-summary-dropdown/item-summary-dropdown.component';
 import { ApprovalLayoutComponent } from './pages/approval-layout/approval-layout.component';
+import { ScrollableDirective } from './scrollable.directive';
 
 @NgModule({
-	declarations: [ApprovalLayoutComponent, ItemSummaryDropdownComponent, AddressButtonNotifComponent],
+	declarations: [ApprovalLayoutComponent, ItemSummaryDropdownComponent, AddressButtonNotifComponent, AddressNotifComponent, ScrollableDirective],
 	imports: [
 		CommonModule,
 		ApprovalRoutingModule,
--- src/app/modules/approval/pages/approval-layout/approval-layout.component.ts
+++ src/app/modules/approval/pages/approval-layout/approval-layout.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'approval-layout',
-  templateUrl: './approval-layout.component.html',
-  styleUrls: ['./approval-layout.component.scss']
+	selector: 'approval-layout',
+	templateUrl: './approval-layout.component.html',
+	styleUrls: ['./approval-layout.component.scss']
 })
 export class ApprovalLayoutComponent implements OnInit {
+	@ViewChild('widgetsContent', { read: ElementRef }) public widgetsContent: ElementRef<any>;
 
-  constructor() { }
+	selected;
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	listApprovals = [
+		{
+			name: 'Kantor Pusat',
+			qty: 10,
+		},
+		{
+			name: 'Kantor Cabang 1',
+			qty: 8,
+		},
+		{
+			name: 'Kantor Cabang 2',
+			qty: 0,
+		},
+		{
+			name: 'Kantor Cabang 3',
+			qty: 0,
+		},
+		{
+			name: 'Kantor Cabang 4',
+			qty: 6,
+		},
+		{
+			name: 'Kantor Cabang 5',
+			qty: 0,
+		},
+		{
+			name: 'Kantor Cabang 6',
+			qty: 2,
+		},
+
+
+	];
+
+
+	diklikEvent(test) {
+		console.log(test);
+		this.selected = test;
+	}
+
+	ngOnInit(): void {
+		this.selected = this.listApprovals[0];
+	}
+
+	scrollLeft() {
+		this.widgetsContent.nativeElement.scrollTo({ left: (this.widgetsContent.nativeElement.scrollLeft - 150), behavior: 'smooth' })
+		console.log('to the left');
+	}
+
+	scrollRight() {
+		this.widgetsContent.nativeElement.scrollTo({ left: (this.widgetsContent.nativeElement.scrollLeft + 150), behavior: 'smooth' })
+		console.log('to the right');
+	}
+
 
 }
