c58524130510ca851cc9dc70a23aeb54ba51742d
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-08-07 14:36:30 +0700

    test

---
+++ src/app/modules/approval/components/item-approval/item-approval.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'item-approval',
+  templateUrl: './item-approval.component.html',
+  styleUrls: ['./item-approval.component.scss']
+})
+export class ItemApprovalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
