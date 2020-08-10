41b38f1ae2793917f966034ed0e45c63af9f442e
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-08-10 14:23:59 +0700

    fix merge

--- src/app/modules/request-approval/pages/request-approval-layout/request-approval-layout.component.ts
+++ src/app/modules/request-approval/pages/request-approval-layout/request-approval-layout.component.ts
@@ -24,10 +24,7 @@ export class RequestApprovalLayoutComponent implements OnInit {
 		history.pushState(null, null, window.location.href);
 
 		location.onPopState(() => {
-<<<<<<< HEAD
 			history.pushState(null, null, window.location.pathname);
-=======
->>>>>>> 99794b8602e4b68dc378f0a76c1d64754e62ee5a
 			this.openDialogLocation('./cart');
 		});
 	}
@@ -60,5 +57,5 @@ export class RequestApprovalLayoutComponent implements OnInit {
 		return false;
 	}
 
-	ngOnInit(): void { }
+	ngOnInit(): void {}
 }
