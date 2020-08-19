293fc7ec6389e683827e56b26da4038fcf760241
Dian Putra <dian.putra@narindo.com>
2020-08-19 15:41:01 +0700

    Revisi 19/08/2020

--- src/app/modules/transaction/components/badge/badge.component.ts
+++ src/app/modules/transaction/components/badge/badge.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class BadgeComponent implements OnInit {
 	@Input() label;
 	@Input() show;
+	@Input() statusprocess;
+
 	constructor() { }
 
 	ngOnInit(): void {
