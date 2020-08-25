61167c188f07b63ec403cb66c53d6521e35865f6
Dian Putra <dian.putra@narindo.com>
2020-08-25 13:53:36 +0700

    Update 2020/08/25 Part 1

--- src/app/modules/transaction-detail/components/rekap-tagihan/rekap-tagihan.component.ts
+++ src/app/modules/transaction-detail/components/rekap-tagihan/rekap-tagihan.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'rekap-tagihan',
-  templateUrl: './rekap-tagihan.component.html',
-  styleUrls: ['./rekap-tagihan.component.scss']
+	selector: 'rekap-tagihan',
+	templateUrl: './rekap-tagihan.component.html',
+	styleUrls: ['./rekap-tagihan.component.scss']
 })
 export class RekapTagihanComponent implements OnInit {
+	username;
+	company;
+	address;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.username = 'Firman Taher';
+		this.company = 'PT Narindo Solusi Telekomunikasi';
+		this.address = 'Jalan Merpati I no. 23, Kemang Jakarta Selatan, 12320 - DKI Jakarta Telp. 0812 3456 7890';
+	}
 
 }
