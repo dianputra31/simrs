31a6df4dd6f1d3cab7ee6133bea85e07e4064084
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-10-02 13:55:21 +0700

    ga penting

--- src/app/modules/cart/pages/cart-layout/cart-layout.component.ts
+++ src/app/modules/cart/pages/cart-layout/cart-layout.component.ts
@@ -16,7 +16,7 @@ export class CartLayoutComponent implements OnInit {
 	subsribers: Subscription[];
 	items: CartListElement[] = [];
 	itemsoriginal: CartListElement[];
-	constructor(private route: ActivatedRoute, private service: BaseService) { }
+	constructor(private route: ActivatedRoute, private service: BaseService) {}
 	isEmpty = 0;
 	company: Company = null;
 
@@ -49,12 +49,20 @@ export class CartLayoutComponent implements OnInit {
 				const stringnya = Convert.cartListToJson(resp);
 				const cartList = Convert.toCartList(stringnya);
 				this.itemsoriginal = cartList.data.cart_list;
+
+				console.log(this.itemsoriginal);
 				this.isEmpty = this.itemsoriginal.length;
 				if (this.isEmpty > 0) {
 					this.pertotalan.totalPrice = cartList.data.total_price;
 					this.pertotalan.totalItem = cartList.data.total_item;
-					for (let index = 0; index < this.itemsoriginal.length; index++) {
-						const element: CartListElement = this.itemsoriginal[index];
+					for (
+						let index = 0;
+						index < this.itemsoriginal.length;
+						index++
+					) {
+						const element: CartListElement = this.itemsoriginal[
+							index
+						];
 						element.outOfStock = element.stock == 0;
 						element.selected = element.stock != 0;
 						element.qtyObject = new QuantityModel();
@@ -67,8 +75,7 @@ export class CartLayoutComponent implements OnInit {
 						this.pertotalan.ongkir += element.shipping_cost;
 					}
 					this.pertotalan.subtotal =
-						cartList.data.total_price +
-						this.pertotalan.totalFee;
+						cartList.data.total_price + this.pertotalan.totalFee;
 					this.pertotalan.grandtotal =
 						this.pertotalan.subtotal +
 						this.pertotalan.ppn +
@@ -109,8 +116,13 @@ export class CartLayoutComponent implements OnInit {
 				this.pertotalan.ppn3 += this.company.pph_percentage;
 				this.pertotalan.ongkir += element.shipping_cost;
 			}
-			this.pertotalan.subtotal = this.pertotalan.totalPrice + this.pertotalan.totalFee;
-			this.pertotalan.grandtotal = this.pertotalan.subtotal + this.pertotalan.ppn + this.pertotalan.ppn3 + this.pertotalan.ongkir;
+			this.pertotalan.subtotal =
+				this.pertotalan.totalPrice + this.pertotalan.totalFee;
+			this.pertotalan.grandtotal =
+				this.pertotalan.subtotal +
+				this.pertotalan.ppn +
+				this.pertotalan.ppn3 +
+				this.pertotalan.ongkir;
 		}
 	}
 
@@ -119,7 +131,6 @@ export class CartLayoutComponent implements OnInit {
 		this.pertotalan.totalPrice = 0;
 		this.pertotalan.totalItem = 0;
 		for (var index in this.items) {
-
 			const element: CartListElement = this.items[index];
 			element.outOfStock = element.stock == 0;
 			element.selected = element.stock != 0;
@@ -134,8 +145,13 @@ export class CartLayoutComponent implements OnInit {
 				adaItemChecked = true;
 			}
 		}
-		this.pertotalan.subtotal = this.pertotalan.totalPrice + this.pertotalan.totalFee;
-		this.pertotalan.grandtotal = this.pertotalan.subtotal + this.pertotalan.ppn + this.pertotalan.ppn3 + this.pertotalan.ongkir;
+		this.pertotalan.subtotal =
+			this.pertotalan.totalPrice + this.pertotalan.totalFee;
+		this.pertotalan.grandtotal =
+			this.pertotalan.subtotal +
+			this.pertotalan.ppn +
+			this.pertotalan.ppn3 +
+			this.pertotalan.ongkir;
 
 		return adaItemChecked;
 	}
