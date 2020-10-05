8a965bedc7b66dfa07f7088c662d284dc129147c
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-10-05 18:34:53 +0700

    date

--- src/app/modules/transaction/components/filter-buttons-section/filter-buttons-section.component.ts
+++ src/app/modules/transaction/components/filter-buttons-section/filter-buttons-section.component.ts
@@ -6,26 +6,31 @@ import { Component, Input, OnInit } from '@angular/core';
 	styleUrls: ['./filter-buttons-section.component.scss'],
 })
 export class FilterButtonsSectionComponent implements OnInit {
-	@Input() param: any
+	@Input() param: any;
 
-	constructor() { }
+	constructor() {}
 
-	ngOnInit(): void { }
+	ngOnInit(): void {}
 
 	test(keyword) {
-		console.log("param: ", this.param)
+		console.log('param: ', this.param);
 		console.log(keyword);
 
-		this.param.keyword = keyword
+		this.param.keyword = keyword;
 
-		console.log("this-keyword: ", this.param.keyword)
+		console.log('this-keyword: ', this.param.keyword);
 	}
 
 	updateUserId(user_id) {
-		this.param.user_id = user_id
+		this.param.user_id = user_id;
 	}
 
 	updateDeliveryAddress(address_id) {
-		this.param.address_id = address_id
+		this.param.address_id = address_id;
+	}
+
+	updateDate(date) {
+		this.param.start_date = date.startdate;
+		this.param.end_date = date.enddate;
 	}
 }
