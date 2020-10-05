2999e6ce581f726a992cbab040ea29166dbd56ed
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-10-05 18:04:14 +0700

    logout

--- src/app/modules/account/components/account-navigation-bar/account-navigation-bar.component.ts
+++ src/app/modules/account/components/account-navigation-bar/account-navigation-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { StorageService } from '../../../../core/storage/service/storage.service';
 
 @Component({
 	selector: 'account-navigation-bar',
@@ -38,13 +40,20 @@ export class AccountNavigationBarComponent implements OnInit {
 	];
 
 	selected;
-	constructor() {}
+	constructor(
+		public storageService: StorageService,
+		@Inject(DOCUMENT) private _document: Document
+	) {}
 
 	ngOnInit(): void {
 		this.selected = this.navItems[0];
 	}
 
 	selectItem(item) {
+		if (item.endPoint == 'logout') {
+			this.storageService.clear();
+			this._document.defaultView.location.reload();
+		}
 		this.selected = item;
 	}
 }
