5f3d4efe2328f16891a80b69e468f4b8a15b4596
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-10-07 18:43:37 +0700

    dialog waiting approval

--- src/app/modules/home/pages/home-layout/home-layout.component.ts
+++ src/app/modules/home/pages/home-layout/home-layout.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material';
 import { Subscription } from 'rxjs';
 import {
+	ApprovalCount,
 	ProductCatalogUrl,
 	ProductTopSubcategoryUrl,
 } from '../../../../app.constant';
@@ -8,6 +10,8 @@ import { BaseService } from '../../../../core/base-service/service/base.service'
 import { ProductCatalogRequestModel } from '../../../../models/product-catalog-request.model';
 import { ProductCatalogResponseModel } from '../../../../models/product-catalog-response-model';
 import { ProductTopSubcategoryResponseModel } from '../../../../models/product-top-subcategory-response.model';
+import { DialogWaitingApprovalComponent } from '../../components/dialog-waiting-approval/dialog-waiting-approval.component';
+
 @Component({
 	selector: 'home-layout',
 	templateUrl: './home-layout.component.html',
@@ -17,12 +21,14 @@ export class HomeLayoutComponent implements OnInit {
 	subsribers: Subscription[];
 	topCategories: ProductTopSubcategoryResponseModel[];
 	productCatalogRows: ProductCatalogResponseModel[][];
-	constructor(private service: BaseService) {}
+	constructor(private service: BaseService, public dialog: MatDialog) {}
 
 	ngOnInit() {
 		this.subsribers = [];
 		this.getProductTopSubcategory();
 		this.productCatalogRows = [];
+
+		this.numberOfApproval();
 	}
 
 	ngOnDestroy() {
@@ -57,6 +63,39 @@ export class HomeLayoutComponent implements OnInit {
 				this.productCatalogRows.push(resp);
 			});
 
+		this.subsribers.push(sub);
+	}
+
+	openDialogManager(tc) {
+		const dialogConfig = new MatDialogConfig();
+		dialogConfig.disableClose = false;
+		dialogConfig.id = 'modal-component';
+		dialogConfig.height = 'auto';
+		dialogConfig.width = '477px';
+		dialogConfig.height = '155px';
+		dialogConfig.panelClass = 'border-radius:20px';
+		dialogConfig.data = {
+			numberOfApproval: tc,
+		};
+
+		const modalDialog = this.dialog.open(
+			DialogWaitingApprovalComponent,
+			dialogConfig
+		);
+
+		return false;
+	}
+
+	numberOfApproval() {
+		const sub = this.service
+			.postData(ApprovalCount, false, false, false)
+			.subscribe((resp) => {
+				var tc = resp.data.approval_count;
+
+				if (tc > 0) {
+					this.openDialogManager(tc);
+				}
+			});
 		this.subsribers.push(sub);
 	}
 }
