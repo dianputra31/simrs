02adf0c149464731e7b7baf4db15b3cac9c79b77
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-10-08 16:35:10 +0700

    cart 2

---
+++ src/app/modules/cart2/pages/cart-layout/cart-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cart-layout',
+  templateUrl: './cart-layout.component.html',
+  styleUrls: ['./cart-layout.component.scss']
+})
+export class CartLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
