cec44c83fdf9c1a8038b0299a0540498409b1daf
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-10-08 12:31:01 +0700

    clean up

--- src/app/shared/components/box-cart-price/box-cart-price.component.ts
+++ src/app/shared/components/box-cart-price/box-cart-price.component.ts
@@ -2,24 +2,20 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { ApprovalUrl, ApproveUrl } from '../../../app.constant';
+import { ApproveUrl } from '../../../app.constant';
 import { BaseService } from '../../../core/base-service/service/base.service';
 import { StorageService } from '../../../core/storage/service/storage.service';
 import { CartListElement } from '../../../models/cart-list.model';
 import {
 	ApproveCartParams,
 	CartListApproveParams,
-	CartListParams,
-	CheckoutCartParams,
 	ConvertApproveParams,
-	ConvertCheckoutParams,
 } from '../../../models/checkout-cart-params.model';
 import {
 	CheckoutCart,
 	ConvertCheckoutCart,
 } from '../../../models/checkout-cart.model';
 import { ApprovalConfirmationDialogComponent } from '../../../modules/approval/components/approval-confirmation-dialog/approval-confirmation-dialog.component';
-import { PopUpRequestApprovalComponent } from '../../../shared/components/pop-up-request-approval/pop-up-request-approval.component';
 
 @Component({
 	selector: 'box-cart-price',
@@ -66,27 +62,9 @@ export class BoxCartPriceComponent implements OnInit {
 			var pm: String = ConvertApproveParams.approveCartParamsToJson(
 				params
 			);
-		} else {
-			const cart_list: CartListParams[] = [];
-
-			for (let index = 0; index < this.selectedItems.length; index++) {
-				const element: CartListElement = this.selectedItems[index];
-				const cart = new CartListParams();
-				cart.product_id = element.product_id;
-				cart.quantity = element.quantity;
-				cart_list.push(cart);
-			}
-			const params: CheckoutCartParams = {
-				cart_list: cart_list,
-			};
-			var pm: String = ConvertCheckoutParams.checkoutCartParamsToJson(
-				params
-			);
 		}
 		var url = '';
-		if (type == 'req-approval') {
-			url = ApprovalUrl;
-		} else if (type == 'proses') {
+		if (type == 'proses') {
 			url = ApproveUrl;
 			if (this.storageService.getRole() != 'Manager') {
 				pm = null;
@@ -100,9 +78,7 @@ export class BoxCartPriceComponent implements OnInit {
 					stringnya
 				);
 				if (cartCheckout.status.rc == 1) {
-					if (type == 'req-approval') {
-						this.openDialogLocation('./cart');
-					} else if (type == 'proses') {
+					if (type == 'proses') {
 						this.router.navigate(['./approval']);
 					}
 				} else {
@@ -110,29 +86,6 @@ export class BoxCartPriceComponent implements OnInit {
 			});
 
 		this.subsribers.push(sub);
-	}
-
-	openDialogLocation(des) {
-		const dialogConfig = new MatDialogConfig();
-		dialogConfig.disableClose = false;
-		dialogConfig.id = 'modal-component';
-		dialogConfig.height = 'auto';
-		dialogConfig.width = '477px';
-		dialogConfig.height = '155px';
-		dialogConfig.panelClass = 'border-radius:20px';
-		dialogConfig.data = {
-			pageBefore: this.router.url,
-			pageDestination: des,
-			modePopUp: '1',
-		};
-		const modalDialog = this.dialog.open(
-			PopUpRequestApprovalComponent,
-			dialogConfig
-		);
-		modalDialog.afterClosed().subscribe((result) => {
-			if (result.event == 'proses') {
-			}
-		});
 	}
 
 	openConfirmDialog(des) {
@@ -165,18 +118,7 @@ export class BoxCartPriceComponent implements OnInit {
 	clickButtonLabel() {
 		this.buttonBoxCartPriceClick.emit();
 		if (this.buttonDisable) {
-			if (this.buttonLabel == 'Selanjutnya123') {
-				this.subsribers = [];
-				this.route.paramMap.subscribe((params) => {
-					this.postCartItem('selanjutnya');
-				});
-			} else if (this.buttonLabel == 'Request Approval') {
-				if (this.selectedItems.length > 0) {
-					this.postCartItem('req-approval');
-				} else {
-					alert('Maaf tidak ada produk yang tersedia');
-				}
-			} else if (this.buttonLabel == 'Proses') {
+			if (this.buttonLabel == 'Proses') {
 				this.openConfirmDialog('./approval');
 			}
 		}
