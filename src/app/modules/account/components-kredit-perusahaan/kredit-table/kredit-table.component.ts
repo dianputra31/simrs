e470438beb5916ac6f7e1a0c3fa9bb8079a2dd6e
Dian Putra <dian.putra@narindo.com>
2020-10-15 18:00:07 +0700

    Kredit Perusahaan, Top Table

--- src/app/modules/account/components-kredit-perusahaan/kredit-table/kredit-table.component.ts
+++ src/app/modules/account/components-kredit-perusahaan/kredit-table/kredit-table.component.ts
@@ -25,7 +25,6 @@ export class KreditTableComponent implements OnInit {
 		const sub = this.service
 			.postData(HistoryMutation, false, false, false)
 			.subscribe((resp) => {
-				console.log(resp.data);
 				var tc = resp.data.length;
 				if (parseInt(tc) > 0) {
 					document.getElementById('item-count').innerText =
@@ -40,8 +39,6 @@ export class KreditTableComponent implements OnInit {
 			});
 		this.subsribers.push(sub);
 	}
-
-	//   http://172.16.204.6:8081/history/mutation?page=1&limit=10
 
 
 
--- src/app/modules/account/pages/account-kredit-perusahaan/account-kredit-perusahaan.component.ts
+++ src/app/modules/account/pages/account-kredit-perusahaan/account-kredit-perusahaan.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ProfileUrl } from '../../../../app.constant';
+import { BaseService } from '../../../../core/base-service/service/base.service';
 
 @Component({
-  selector: 'account-kredit-perusahaan',
-  templateUrl: './account-kredit-perusahaan.component.html',
-  styleUrls: ['./account-kredit-perusahaan.component.scss']
+	selector: 'account-kredit-perusahaan',
+	templateUrl: './account-kredit-perusahaan.component.html',
+	styleUrls: ['./account-kredit-perusahaan.component.scss']
 })
 export class AccountKreditPerusahaanComponent implements OnInit {
+	limitkredit;
+	sisakreditnya;
+	subsribers: Subscription[];
 
-  constructor() { }
+	constructor(
+		private service: BaseService,
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.subsribers = [];
+		const sub = this.service
+			.getData(ProfileUrl, false, null, true)
+			.subscribe((resp) => {
+				console.log(resp.data.company);
+				this.limitkredit = resp.data.company.credit_limit;
+				this.sisakreditnya = resp.data.company.credit_rp;
+			});
+		this.subsribers.push(sub);
+
+
+
+	}
 
 }
