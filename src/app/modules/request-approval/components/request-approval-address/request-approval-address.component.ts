70c015919c22aa2abbf64f6b417dbd9721cc7b85
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-10-16 11:47:05 +0700

    purchaser name

--- src/app/modules/request-approval/components/request-approval-address/request-approval-address.component.ts
+++ src/app/modules/request-approval/components/request-approval-address/request-approval-address.component.ts
@@ -2,44 +2,53 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ProfileUrl } from '../../../../app.constant';
 import { BaseService } from '../../../../core/base-service/service/base.service';
+import { StorageService } from '../../../../core/storage/service/storage.service';
 import { ProfileResponseModel } from '../../../../shared/components/address-section/model/profile-response.model';
-
 
 @Component({
 	selector: 'request-approval-address',
 	templateUrl: './request-approval-address.component.html',
-	styleUrls: ['./request-approval-address.component.scss']
+	styleUrls: ['./request-approval-address.component.scss'],
 })
 export class RequestApprovalAddressComponent implements OnInit {
 	username;
+	fullname;
 	address;
 	addresses;
 	phone;
 	province;
 	subsribers: Subscription[];
 
-	constructor(private service: BaseService) { }
+	constructor(
+		private service: BaseService,
+		private storageService: StorageService
+	) {}
 
 	ngOnInit(): void {
-
-
+		this.fullname = this.storageService.fullname;
 		const url = ProfileUrl;
 		const sub = this.service
 			.getData(url, ProfileResponseModel, null, false)
 			.subscribe((resp) => {
 				this.addresses = resp.delivery_address;
 
-				var def_addr = this.addresses[0].address_name + " - " + this.addresses[0].address_detail
+				var def_addr =
+					this.addresses[0].address_name +
+					' - ' +
+					this.addresses[0].address_detail;
 				this.username = this.addresses[0].address_name;
-				this.address = this.addresses[0].address_detail + ', ' + this.addresses[0].village + ', ' + this.addresses[0].subdistrict +
-					', ' + this.addresses[0].district;
+				this.address =
+					this.addresses[0].address_detail +
+					', ' +
+					this.addresses[0].village +
+					', ' +
+					this.addresses[0].subdistrict +
+					', ' +
+					this.addresses[0].district;
 				this.province = this.addresses[0].province;
 				this.phone = 'Telp. ' + this.addresses[0].recipient_contact;
-
 			});
 
 		this.subsribers.push(sub);
-
 	}
-
 }
