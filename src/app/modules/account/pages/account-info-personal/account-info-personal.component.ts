acbe0637333902fa52f9a309dbf591711e688e5c
Dian Putra <dian.putra@narindo.com>
2020-10-20 16:07:07 +0700

    Revisi 20/10/2020 Part 1

--- src/app/modules/account/pages/account-info-personal/account-info-personal.component.ts
+++ src/app/modules/account/pages/account-info-personal/account-info-personal.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { EditProfile, ProfileUrl } from '../../../../app.constant';
+import { HttpService } from '../../../../core/base-service/http.service';
+import { BaseService } from '../../../../core/base-service/service/base.service';
 
 @Component({
 	selector: 'account-info-personal',
@@ -7,10 +10,16 @@ import { Subscription } from 'rxjs';
 	styleUrls: ['./account-info-personal.component.scss'],
 })
 export class AccountInfoPersonalComponent implements OnInit {
+	@Input() firstnameEdit: String;
+	@Input() lastnameEdit: String;
+
 	datausr;
 	isEditting;
 	firstName;
 	lastName;
+	firstname;
+	lastname;
+	email;
 
 	param =
 		{
@@ -23,28 +32,55 @@ export class AccountInfoPersonalComponent implements OnInit {
 			profile_picture_url: ""
 		}
 
-	constructor() { }
+	constructor(private http: HttpService, private service: BaseService) { }
 
 	subsribers: Subscription[];
 
 	ngOnInit(): void {
 		this.subsribers = [];
-		var datausr = JSON.parse(localStorage.getItem('profile'));
-		var splitted = datausr.fullname.split(" ", 2);
-		this.firstName = splitted[0];
-		this.lastName = splitted[1];
 
-		this.param.email = datausr.email;
-		this.isEditting = false;
+
+		const sub = this.http.get(ProfileUrl, '').subscribe(
+			resp => {
+				var datausr = resp.data.profile;
+				this.firstName = datausr.fullname.split(' ').slice(0, -1).join(' ');;
+				this.lastName = datausr.fullname.split(' ').slice(-1).join(' ');
+				this.email = datausr.email;
+
+				this.param.email = datausr.email;
+				this.param.first_name = this.firstName;
+				this.param.last_name = this.lastName;
+				this.param.role_id = datausr.role_id;
+				this.param.company_id = datausr.company_id;
+				this.param.profile_picture_url = datausr.profile_picture_url;
+				this.isEditting = false;
+			}
+		)
+
+
+
 
 	}
 
-	save(a, b) {
+	save() {
 		this.isEditting = false;
-		console.log(a + ' ' + b);
+
+		this.http.post(EditProfile + this.param.email, this.param).subscribe(resp => {
+			window.location.reload();
+		})
 	}
+
 
 	edit() {
 		this.isEditting = true;
 	}
+
+	firstnameChange(event) {
+		this.param.first_name = event;
+	}
+
+	lastnameChange(event) {
+		this.param.last_name = event;
+	}
+
 }
