3856510b7f9fa610ddaae99e4f317be228b88c52
Dian Putra <dian.putra@narindo.com>
2020-11-02 14:00:23 +0700

    [Informasi Personal] Display Name Is Not Updated

--- src/app/modules/account/pages/account-layout/account-layout.component.ts
+++ src/app/modules/account/pages/account-layout/account-layout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { ProfileUrl } from '../../../../app.constant';
+import { HttpService } from '../../../../core/base-service/http.service';
 
 
 @Component({
@@ -12,17 +14,24 @@ export class AccountLayoutComponent implements OnInit {
 	datauser;
 	account;
 
-	constructor() { }
+	constructor(private http: HttpService, ) { }
 
 	subsribers: Subscription[];
 
 	ngOnInit(): void {
 		this.subsribers = [];
 		this.datacompany = JSON.parse(localStorage.getItem('company'));
-		this.datauser = JSON.parse(localStorage.getItem('profile'));
 		this.account = JSON.parse(localStorage.getItem('account'));
-	}
 
+		const sub = this.http.get(ProfileUrl, '').subscribe(
+			resp => {
+				this.datauser = resp.data.profile;
+
+			}
+		)
+
+	}
+ 
 
 
 }
