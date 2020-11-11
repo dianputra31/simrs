acda221531a4560e47c2183855c7f4acd4a35141
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-11-11 18:14:32 +0700

    fix bug

--- src/app/modules/cart2/components/item-card/item-card.component.ts
+++ src/app/modules/cart2/components/item-card/item-card.component.ts
@@ -3,10 +3,9 @@ import { Router } from '@angular/router';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
 import { Subscription } from 'rxjs';
 import { AddCart } from '../../../../app.constant';
-import { BaseService } from '../../../../core/base-service/service/base.service';
+import { HttpService } from '../../../../core/base-service/http.service';
 import { RedirectParameterService } from '../../../../layout/redirect-parameter.service';
 import { CartItemRequestModel } from '../../../../models/cart-item-request.model';
-import { CartItemResponseModel } from '../../../../models/cart-item-response.model';
 import { CartItemModel } from '../../../../models/cart-item.model';
 import { CartListItemModel } from '../../../../models/cart-list-item.model';
 import { ToastService } from '../../../../shared/toast/toast-service';
@@ -20,11 +19,12 @@ import { ITEM_AVAILABILITY } from '../../cart.constant';
 export class ItemCardComponent implements OnInit {
 	@Input() item: CartListItemModel;
 	@Output() onUpdateQty = new EventEmitter();
+	@Output() onDeleteItem = new EventEmitter();
 	@BlockUI() blockUI: NgBlockUI;
 
 	subscribers: Subscription[];
 	constructor(
-		public service: BaseService,
+		public service: HttpService,
 		public toastService: ToastService,
 		private _redirectparam: RedirectParameterService,
 		private router: Router
@@ -67,12 +67,11 @@ export class ItemCardComponent implements OnInit {
 		cartreq.cart_list = [];
 		cartreq.cart_list.push(test);
 		this.blockUI.start();
-		const sub = this.service
-			.postData(AddCart, cartreq, CartItemResponseModel, false)
-			.subscribe((resp) => {
-				this.blockUI.stop();
-				this.onUpdateQty.emit();
-			});
+		const sub = this.service.post(AddCart, cartreq).subscribe((resp) => {
+			this.blockUI.stop();
+			console.log(resp);
+			this.onUpdateQty.emit(resp.data[0]);
+		});
 
 		this.subscribers.push(sub);
 	}
@@ -86,13 +85,11 @@ export class ItemCardComponent implements OnInit {
 		cartreq.cart_list = [];
 		cartreq.cart_list.push(test);
 		this.blockUI.start();
-		const sub = this.service
-			.postData(AddCart, cartreq, CartItemResponseModel, false)
-			.subscribe((resp) => {
-				this.blockUI.stop();
-				this.showDanger(dangerTpl);
-				this.onUpdateQty.emit();
-			});
+		const sub = this.service.post(AddCart, cartreq).subscribe((resp) => {
+			this.blockUI.stop();
+			this.showDanger(dangerTpl);
+			this.onDeleteItem.emit();
+		});
 		this.subscribers.push(sub);
 	}
 
