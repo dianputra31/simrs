ef84149bac7bd036751649cfd27965a862648b72
Sukhito Tanawal <sukhito.tanawal@gmail.com>
2020-11-30 10:34:33 +0700

    here

---
+++ src/app/layout/pages/login-layout3/login-layout3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'login-layout3',
+  templateUrl: './login-layout3.component.html',
+  styleUrls: ['./login-layout3.component.scss']
+})
+export class LoginLayout3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
